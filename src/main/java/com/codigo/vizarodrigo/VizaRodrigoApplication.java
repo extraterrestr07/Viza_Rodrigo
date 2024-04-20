7b5fcc146c6767d80499b0a04f799ec0fbe9d0d3
Rodrigo Viza <rodrigoestefanovizacuti@gmail.com>
2024-04-19 22:53:44 -0500

    creando Proyecto

---
+++ src/main/java/com/codigo/vizarodrigo/VizaRodrigoApplication.java
@@ -0,0 +1,13 @@
+package com.codigo.vizarodrigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VizaRodrigoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VizaRodrigoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codigo/vizarodrigo/VizaRodrigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo.vizarodrigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VizaRodrigoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
